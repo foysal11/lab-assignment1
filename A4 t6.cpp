62ffad661e6fb3bc62faf111836bbe0945941e34
foysal11 <32926097+foysal11@users.noreply.github.com>
2017-10-26 23:34:29 +0600

    Create A4 t6.cpp

---
+++ A4 t6.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[10];
+
+    int n;
+
+    for(int i=0;i<10;++i){
+        scanf("%d",&arr[i]);
+
+    }
+   for(int i=0;i<10;++i){
+       printf("%d ",arr[i]);
+
+    }
+}
